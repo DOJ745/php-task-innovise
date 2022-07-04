8057c6747b36866d3869de15b04cc7942f01f1c7
DOJ745 <andreyfa12@mail.ru>
2022-07-04 19:34:03 +0300

    Task 3 has been completed

--- src/Task3.php
+++ src/Task3.php
@@ -3,10 +3,38 @@
 namespace src;
 
 use InvalidArgumentException;
+use TypeError;
 
 class Task3
 {
-    function main() : void{
-        throw new InvalidArgumentException();
+    private function countNumSum(int $num): int{
+        $absNum = (string) abs($num);
+        $strLen = strlen($absNum);
+        $result = 0;
+        for($i = $strLen - 1; $i >= 0; $i--){
+            $result += (int) $absNum[$i];
+        }
+        return $result;
     }
+
+    public function main(int $num) : int {
+        if(gettype($num) !== 'integer') throw new InvalidArgumentException("Use number!");
+        $numLength = strlen((string) $this->countNumSum($num));
+        $result = $this->countNumSum($num);
+        while ($numLength > 1){
+            $numLength = strlen((string) $this->countNumSum($num));
+            $result = $this->countNumSum($num);
+            $num = $result;
+            print_r("Step: " . $num . "\n");
+        }
+        return $result;
+    }
+}
+
+$test = new Task3();
+try{
+    print_r("Final: " . $test->main("456"));
+}
+catch (TypeError $e){
+    print_r($e->getMessage());
 }
