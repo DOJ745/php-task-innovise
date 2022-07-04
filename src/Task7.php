1a7d37d6954be99524bbb6ebb008b13b51688012
DOJ745 <andreyfa12@mail.ru>
2022-07-04 20:34:29 +0300

    Task 7 has been completed

--- src/Task7.php
+++ src/Task7.php
@@ -3,10 +3,23 @@
 namespace src;
 
 use InvalidArgumentException;
+use TypeError;
 
 class Task7
 {
-    function main() : void{
-        throw new InvalidArgumentException();
+    function main(array $arr, int $position) : array{
+        if(gettype($arr) !== "array") throw new InvalidArgumentException("Argument must be an array!");
+        array_splice($arr, $position, 1);
+        return $arr;
     }
+}
+
+$test = new Task7();
+
+try{
+    $testArr = array(1, 5, 7, 8, 10, 2, 3);
+    print_r($test->main($testArr, 4));
+}
+catch (TypeError $e){
+    print_r($e->getMessage());
 }
