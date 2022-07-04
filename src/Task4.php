06f6055854a6db0114784347c8322facf0f4ae93
DOJ745 <andreyfa12@mail.ru>
2022-07-04 19:55:26 +0300

    Task 4 has been completed

--- src/Task4.php
+++ src/Task4.php
@@ -3,10 +3,30 @@
 namespace src;
 
 use InvalidArgumentException;
+use TypeError;
 
 class Task4
 {
-    function main() : void{
-        throw new InvalidArgumentException();
+    function main(string $input) : string{
+        if(gettype($input) !== 'string') throw new InvalidArgumentException();
+        $result = '';
+        $strLen = strlen($input);
+
+        for($i = 0; $i < $strLen; $i++){
+            if($input[$i] === "_" || $input[$i] === "-" || $input[$i] === " "){
+                $result .= strtoupper($input[$i + 1]);
+                $i += 2;
+            }
+            $result .= $input[$i];
+        }
+        return $result;
     }
+}
+
+$test = new Task4();
+try{
+    print_r($test->main("The quick-brown_fox jumps over the_lazy-dog"));
+}
+catch (TypeError $e){
+    print_r($e->getMessage());
 }
