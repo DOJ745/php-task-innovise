e6ebbe01f9af4a5a32bf6211e0f8e2ce35aae59e
DOJ745 <andreyfa12@mail.ru>
2022-07-04 20:53:40 +0300

    Task 10 has been completed

--- src/Task10.php
+++ src/Task10.php
@@ -2,11 +2,32 @@
 
 namespace src;
 
+use Exception;
 use InvalidArgumentException;
+use TypeError;
 
 class Task10
 {
-    function main() : void{
-        throw new InvalidArgumentException();
+    function main(int $input) : array{
+        if(gettype($input) !== "integer" || $input < 0) throw new InvalidArgumentException("Argument must be a positive number!");
+        $result = array($input);
+        while($input > 1){
+            if($input % 2 === 0) $input /= 2;
+            else $input = $input * 3 + 1;
+
+            $result[] = $input;
+        }
+        return $result;
     }
+}
+
+$test = new Task10();
+try{
+    print_r($test->main(67));
+}
+catch(TypeError $e){
+    print_r($e->getMessage());
+}
+catch (Exception $e){
+    print_r($e->getMessage());
 }
