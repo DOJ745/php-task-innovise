bd6151b4f5809a2f356d0b32e8e43ea098ca778c
DOJ745 <andreyfa12@mail.ru>
2022-07-04 15:02:59 +0300

    Task 1 has been completed

--- src/Task1.php
+++ src/Task1.php
@@ -3,11 +3,24 @@
 namespace src;
 
 use InvalidArgumentException;
+use TypeError;
 
 class Task1
 {
-    public function main(): void
+    public function main(int $inputNumber): string
     {
-        throw new InvalidArgumentException();
+        if(gettype($inputNumber) !== "integer") throw new InvalidArgumentException("Argument must be a number!");
+        // To be honest, such many ternary operators can break someone's head
+        return $inputNumber > 30 ? "More than 30" :
+            ($inputNumber > 20 ? "More than 20" : ($inputNumber > 10 ? "More than 10" : "Equals or less than 10"));
     }
 }
+
+$test = new Task1();
+try {
+    $arg = 21;
+    print_r($test->main($arg));
+} catch (TypeError $e) {
+    print_r($e->getMessage());
+}
+
