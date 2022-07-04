f28389261cff9af7f7fe5bf42f9870376105740f
DOJ745 <andreyfa12@mail.ru>
2022-07-04 21:40:42 +0300

    Task 8 has been completed

--- src/Task8.php
+++ src/Task8.php
@@ -2,11 +2,47 @@
 
 namespace src;
 
+use Exception;
 use InvalidArgumentException;
 
 class Task8
 {
-    function main() : void{
-        throw new InvalidArgumentException();
+    private function formKeyValuePair(string $key, string $value): string{
+        return "$key: $value</br>";
     }
+
+    private function formJsonDoc(string ...$pair): string{
+        return implode('', $pair);
+    }
+
+    public function main(string $json) : string{
+        $errorMsg = "Json cannot be decoded or encoded data is deeper than the nesting limit!";
+        if(json_decode($json) === false || json_decode($json) === null) throw new InvalidArgumentException($errorMsg);
+        $jsonObj = json_decode($json);
+
+        $titleKey = "Title";
+        $titleValue = $jsonObj->{$titleKey};
+        $titlePair = $this->formKeyValuePair($titleKey, $titleValue);
+
+        $authorKey = "Author";
+        $authorValue = $jsonObj->{$authorKey};
+        $authorPair = $this->formKeyValuePair($authorKey, $authorValue);
+
+        $detailKey = "Detail";
+        $publisherKey = "Publisher";
+        $publisherValue = $jsonObj->{$detailKey}->{$publisherKey};
+        $publisherPair = $this->formKeyValuePair($publisherKey, $publisherValue);
+
+        return $this->formJsonDoc($titlePair, $authorPair, $publisherPair);
+    }
+}
+
+$testJson = '{"Title": "The Cuckoos Calling","Author": "Robert Galbraith","Detail": {"Publisher": "Little Brown"}}';
+$test = new Task8();
+
+try{
+    print_r($test->main($testJson));
+}
+catch (Exception $e){
+    print_r($e->getMessage());
 }
