d0c86e30aa46a1e35ffef54039853f77f257de01
DOJ745 <andreyfa12@mail.ru>
2022-07-05 10:22:34 +0300

    Summary: - Fixed PHP CS Fixer - Cosmetic changes

--- src/Task4.php
+++ src/Task4.php
@@ -7,26 +7,30 @@ use TypeError;
 
 class Task4
 {
-    function main(string $input) : string{
-        if(gettype($input) !== 'string') throw new InvalidArgumentException();
+    public function main(string $input): string
+    {
+        if (gettype($input) !== 'string') {
+            throw new InvalidArgumentException('Argument must be a string!');
+        }
         $result = '';
         $strLen = strlen($input);
 
-        for($i = 0; $i < $strLen; $i++){
-            if($input[$i] === "_" || $input[$i] === "-" || $input[$i] === " "){
+        for ($i = 0; $i < $strLen; $i++) {
+            if ($input[$i] === '_' || $input[$i] === '-' || $input[$i] === ' ') {
                 $result .= strtoupper($input[$i + 1]);
                 $i += 2;
             }
             $result .= $input[$i];
         }
+
         return $result;
     }
 }
 
 $test = new Task4();
-try{
-    print_r($test->main("The quick-brown_fox jumps over the_lazy-dog"));
-}
-catch (TypeError $e){
+
+try {
+    print_r($test->main('The quick-brown_fox jumps over the_lazy-dog'));
+} catch (TypeError $e) {
     print_r($e->getMessage());
 }
