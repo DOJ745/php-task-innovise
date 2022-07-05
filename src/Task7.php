44e8ef1a775d8ff2af31bc3a4596a9341d69397c
DOJ745 <andreyfa12@mail.ru>
2022-07-05 10:23:54 +0300

    Summary: - Fixed PHP CS Fixer - Cosmetic changes

--- src/Task7.php
+++ src/Task7.php
@@ -7,19 +7,22 @@ use TypeError;
 
 class Task7
 {
-    function main(array $arr, int $position) : array{
-        if(gettype($arr) !== "array") throw new InvalidArgumentException("Argument must be an array!");
+    public function main(array $arr, int $position): array
+    {
+        if (gettype($arr) !== 'array') {
+            throw new InvalidArgumentException('Argument must be an array!');
+        }
         array_splice($arr, $position, 1);
+
         return $arr;
     }
 }
 
 $test = new Task7();
 
-try{
-    $testArr = array(1, 5, 7, 8, 10, 2, 3);
+try {
+    $testArr = [1, 5, 7, 8, 10, 2, 3];
     print_r($test->main($testArr, 4));
-}
-catch (TypeError $e){
+} catch (TypeError $e) {
     print_r($e->getMessage());
 }
