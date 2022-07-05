de98b0dd175ce9a5d400e34cef6f402f17cbe38c
DOJ745 <andreyfa12@mail.ru>
2022-07-05 19:09:50 +0300

    Trying to match PHP CS Fixer

--- src/Task7.php
+++ src/Task7.php
@@ -6,7 +6,7 @@ class Task7
     public function main(array $arr, int $position): array
     {
         if (gettype($arr) !== 'array') {
-            throw new InvalidArgumentException('Argument must be an array!');
+            throw new \InvalidArgumentException('Argument must be an array!');
         }
         array_splice($arr, $position, 1);
 
