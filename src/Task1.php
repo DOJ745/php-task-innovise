e207f36939edef759f1b2eb892d11339a6a8b542
DOJ745 <andreyfa12@mail.ru>
2022-07-05 10:13:57 +0300

    Fixed PHP CS Fixer

--- src/Task1.php
+++ src/Task1.php
@@ -9,18 +9,20 @@ class Task1
 {
     public function main(int $inputNumber): string
     {
-        if(gettype($inputNumber) !== "integer") throw new InvalidArgumentException("Argument must be a number!");
+        if (gettype($inputNumber) !== 'integer') {
+            throw new InvalidArgumentException('Argument must be a number!');
+        }
         // To be honest, such many ternary operators can break someone's head
-        return $inputNumber > 30 ? "More than 30" :
-            ($inputNumber > 20 ? "More than 20" : ($inputNumber > 10 ? "More than 10" : "Equals or less than 10"));
+        return $inputNumber > 30 ? 'More than 30' :
+            ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
     }
 }
 
 $test = new Task1();
+
 try {
     $arg = 21;
     print_r($test->main($arg));
 } catch (TypeError $e) {
     print_r($e->getMessage());
 }
-
