ba021992bf281243bad7a30c56279272abc0b169
DOJ745 <andreyfa12@mail.ru>
2022-07-05 15:42:16 +0300

    Did some for tests

--- src/Task5.php
+++ src/Task5.php
@@ -1,12 +1,30 @@
-<?php
-
 namespace src;
-
-use InvalidArgumentException;
+<?php
 
 class Task5
 {
-    function main() : void{
-        throw new InvalidArgumentException();
+    private function getFib($n): float
+    {
+        return round(pow((sqrt(5) + 1) / 2, $n) / sqrt(5));
+    }
+
+    private function fib2($n, $c = 2, $n2 = 0, $n1 = 1)
+    {
+        return $c < $n ? $this->fib2($n, $c + 1, $n1, $n1 + $n2) : $n1 + $n2;
+    }
+
+    public function main(int $n): string
+    {
+        $i = 1;
+        $result = 0.0;
+        while (strlen((string) $this->fib2($i)) <= $n) {
+            $result = $this->fib2($i);
+            $i++;
+        }
+
+        return $result;
     }
 }
+
+$test = new Task5();
+print_r($test->main(18));
