626548a2f791bf6d93058509cd1aa3fdb2a10947
DOJ745 <andreyfa12@mail.ru>
2022-07-06 10:39:41 +0300

    Minor changes

--- src/Task1.php
+++ src/Task1.php
@@ -9,7 +9,6 @@ class Task1
             throw new \InvalidArgumentException('Argument must be a number!');
         }
 
-        return $inputNumber > 30 ? 'More than 30' :
-            ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
+        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
     }
 }
