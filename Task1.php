2a0d7b951fe9c66efb26eec217dbf7da0f25085e
DOJ745 <andreyfa12@mail.ru>
2022-07-07 18:55:57 +0300

    Fixed exception

--- Task1.php
+++ Task1.php
@@ -5,8 +5,9 @@ class Task1
 {
     public static function main($inputNumber): string
     {
-        if (gettype($inputNumber) !== 'integer') {
-            throw new \InvalidArgumentException();
+        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
+            throw new InvalidArgumentException();
+            //return InvalidArgumentException::class;
         }
 
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
