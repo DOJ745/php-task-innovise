454b5328e2920dcb851a3d8a386e25f026e25c76
DOJ745 <andreyfa12@mail.ru>
2022-07-07 21:26:55 +0300

    Modified checking for exception

--- Task1.php
+++ Task1.php
@@ -5,9 +5,8 @@ class Task1
 {
     public static function main($inputNumber): string
     {
-        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
-            //throw new InvalidArgumentException();
-            return InvalidArgumentException::class;
+        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber) || empty($inputNumber)) {
+            throw new InvalidArgumentException();
         }
 
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
