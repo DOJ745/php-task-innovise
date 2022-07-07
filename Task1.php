a9757b78de1c83f5d6fef4fbe4863676f4f8b824
DOJ745 <andreyfa12@mail.ru>
2022-07-07 19:09:07 +0300

    Try to return exception class

--- Task1.php
+++ Task1.php
@@ -13,5 +13,3 @@ class Task1
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
     }
 }
-
-print_r(Task1::main('123'));
