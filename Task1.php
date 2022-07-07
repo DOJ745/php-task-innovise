cd2b1f918ac1ac3246e8511d15c83792e2026060
DOJ745 <andreyfa12@mail.ru>
2022-07-07 17:42:53 +0300

    Changed exception

--- Task1.php
+++ Task1.php
@@ -1,12 +1,11 @@
-namespace src;
-<?php
+<?php namespace src;
 
 class Task1
 {
     public static function main($inputNumber): string
     {
         if (gettype($inputNumber) !== 'integer') {
-            throw new \InvalidArgumentException('Argument must be a number!');
+            throw new \InvalidArgumentException();
         }
 
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
