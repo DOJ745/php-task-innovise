9720ed07d69730f41239aeea93411ed9b3e18e71
DOJ745 <andreyfa12@mail.ru>
2022-07-07 19:06:07 +0300

    Try to return exception class

--- Task1.php
+++ Task1.php
@@ -6,10 +6,12 @@ class Task1
     public static function main($inputNumber): string
     {
         if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
-            throw new InvalidArgumentException();
-            //return InvalidArgumentException::class;
+            //throw new InvalidArgumentException();
+            return InvalidArgumentException::class;
         }
 
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
     }
 }
+
+print_r(Task1::main('123'));
