4936cf163b75245e6571a06e60d972f709d82157
DOJ745 <andreyfa12@mail.ru>
2022-07-08 14:59:27 +0300

    Still fixing namespace

--- Task1.php
+++ Task1.php
@@ -1,15 +1,14 @@
 <?php
 
-namespace src {
-    class Task1
+namespace src;
+class Task1
+{
+    public static function main($inputNumber): string
     {
-        public static function main($inputNumber): string
-        {
-            if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
-                throw new \InvalidArgumentException();
-            }
-
-            return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
+        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
+            throw new \InvalidArgumentException();
         }
+
+        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
     }
 }
