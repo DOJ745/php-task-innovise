c373e5807f97372edf76cd4235c79d133502ef96
DOJ745 <andreyfa12@mail.ru>
2022-07-08 09:25:26 +0300

    Defined 'use InvalidArgumentException'

--- Task1.php
+++ Task1.php
@@ -1,12 +1,14 @@
 namespace src;
 <?php
 
+use InvalidArgumentException;
+
 class Task1
 {
     public static function main($inputNumber): string
     {
-        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber) || empty($inputNumber)) {
-            throw new InvalidArgumentException();
+        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
+            throw new \InvalidArgumentException();
         }
 
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
