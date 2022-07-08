e4935731dc276c948fadee572f17d57d6563d850
DOJ745 <andreyfa12@mail.ru>
2022-07-08 12:06:25 +0300

    Fixed namespace, I guess

--- Task1.php
+++ Task1.php
@@ -1,14 +1,15 @@
 <?php
-namespace src;
 
-class Task1
-{
-    public static function main($inputNumber): string
+namespace src {
+    class Task1
     {
-        if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
-            throw new \InvalidArgumentException();
-        }
+        public static function main($inputNumber): string
+        {
+            if (gettype($inputNumber) !== 'integer' || !is_int($inputNumber)) {
+                throw new \InvalidArgumentException();
+            }
 
-        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
+            return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equals or less than 10'));
+        }
     }
 }
